01a2c245da1e778da056ee8e6f095d11cd754411
ironicicon <ramkumar.srv@gmail.com>
2015-01-27 09:53:15 +0530

    Initial Commit of TemplATon
    Created NavBar.cshtml

---
+++ CSharp/TemplATon/MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UserModel _model = new UserModel();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            //actual isUserAuthenticated must come by checking the username and password and username is not empty. (ToDo later)
+            //_model.isUserAuthenticated = false;
+            _model.isUserAuthenticated = true;
+            _model.username = "Ram";
+            return View(_model);
+        }
+
+    }
+}
---
+++ CSharp/TemplATon/MvcApplication1/MvcApplication1/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class UserModel
+    {
+        public bool isUserAuthenticated{get;set;}
+
+        [Required(ErrorMessage="Username cannot be empty")]
+        [MinLength(3,ErrorMessage="Username must be greater than 3 characters")]
+        [Display(Name = "Username")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "password cannot be empty")]
+        [MinLength(3, ErrorMessage = "password must be greater than 3 characters")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
