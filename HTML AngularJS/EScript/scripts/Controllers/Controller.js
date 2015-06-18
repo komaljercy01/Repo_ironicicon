8ba2aadd2fb98f41c425bef61269f1a1952ac1a1
ironicicon <ramkumar.srv@gmail.com>
2015-06-18 19:45:49 +0530

    Update Controller.js
    Updating Controller.js with placeholder text

--- HTML AngularJS/EScript/scripts/Controllers/Controller.js
+++ HTML AngularJS/EScript/scripts/Controllers/Controller.js
@@ -1,4 +1,13 @@
 angular.module('MainApp.controllers',[])
-        .controller('HomeController', function ($scope) {
-                $scope.Message = "I nailed it :P";
-        });
+    .controller('HomeController', function ($scope) {
+            $scope.Message = "I nailed it :P";
+    });
+    .controller('AboutUsController', function ($scope) {
+            $scope.Message = "About Me? .. Buhahahaha :P";
+    });
+    .controller('LoginController', function ($scope) {
+            $scope.Message = "Login ah?.. ada poda";
+    });
+    .controller('ContactUsController', function ($scope) {
+            $scope.Message = "Call us or mail us.. but we wont give details";
+    });
