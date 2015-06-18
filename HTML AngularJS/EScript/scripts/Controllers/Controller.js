b720083aec01e839c5991da48d8b93c85e3b8dab
ironicicon <ramkumar.srv@gmail.com>
2015-06-18 19:51:32 +0530

    Update Controller.js

--- HTML AngularJS/EScript/scripts/Controllers/Controller.js
+++ HTML AngularJS/EScript/scripts/Controllers/Controller.js
@@ -2,12 +2,13 @@ angular.module('MainApp.controllers',[])
     .controller('HomeController', function ($scope) {
             $scope.Message = "I nailed it :P";
     });
+angular.module('MainApp.controllers',[])
     .controller('AboutUsController', function ($scope) {
             $scope.Message = "About Me? .. Buhahahaha :P";
     });
-    .controller('LoginController', function ($scope) {
+angular.module('MainApp.controllers',[]).controller('LoginController', function ($scope) {
             $scope.Message = "Login ah?.. ada poda";
     });
-    .controller('ContactUsController', function ($scope) {
+angular.module('MainApp.controllers',[]).controller('ContactUsController', function ($scope) {
             $scope.Message = "Call us or mail us.. but we wont give details";
     });
