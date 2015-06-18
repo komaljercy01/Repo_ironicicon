99da4e776fb8edc61fba2945936ad3a9fa2e9dca
ironicicon <ramkumar.srv@gmail.com>
2015-06-18 22:27:19 +0530

    Update Controller.js

--- HTML AngularJS/EScript/scripts/Controllers/Controller.js
+++ HTML AngularJS/EScript/scripts/Controllers/Controller.js
@@ -1,6 +1,7 @@
 var controllers=angular.module('MainApp.controllers',[]);
     controllers.controller('HomeController', function ($scope) {
-            $scope.Message = "Welcome To <b>EScript</b>";
+            //$sce.trustAsHtml -- Render HTML String
+            $scope.Message = $sce.trustAsHtml("Welcome To <b>EScript</b>");
     });
     controllers.controller('AboutUsController', function ($scope) {
             $scope.Message = "About Me? .. Buhahahaha :P";
