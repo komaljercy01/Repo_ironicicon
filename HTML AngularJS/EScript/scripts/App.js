c6647c46dc0f323eee48e905c532d7fbc378c878
ironicicon <ramkumar.srv@gmail.com>
2015-06-18 15:26:38 +0530

    Routing Working

--- HTML AngularJS/EScript/scripts/App.js
+++ HTML AngularJS/EScript/scripts/App.js
@@ -1,1 +1,24 @@
-﻿var MainApp = angular.module('MainApp', ['MainApp.controllers']);
+﻿var MainApp = angular.module('MainApp', ['ngRoute']);
+//['MainApp.controllers']
+MainApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/Home', {
+            templateUrl: 'templates/Home.html',
+            controller:'HomeController'
+        })
+        .when('/AboutUs', {
+            templateUrl: 'templates/AboutUs.html',
+            controller: 'AboutUsController'
+        })
+        //.otherwise({
+        //    redirectTo: '/Home'
+        //});
+});
+
+MainApp.controller('HomeController', function ($scope) {
+        $scope.Message = "Welcome To EScript";
+    });
+
+MainApp.controller('AboutUsController', function ($scope) {
+    $scope.Message = "Welcome To About Us Page";
+});
