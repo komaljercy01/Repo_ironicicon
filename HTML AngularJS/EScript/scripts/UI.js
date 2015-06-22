13c8866d9be28ad7aba9a31b24f2c39579e21fff
ironicicon <ramkumar.srv@gmail.com>
2015-06-22 22:23:51 +0530

    Create UI.js

---
+++ HTML AngularJS/EScript/scripts/UI.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(window).scroll(function() {
+    if ($(".nav").offset().top > 50) {
+        $(".nav").addClass("top-nav-collapse");
+    } else {
+        $(".nav").removeClass("top-nav-collapse");
+    }
+});
+});
