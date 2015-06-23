b8c82b32bd89d3b36a073df527b0e1c8203f6ce4
ironicicon <ramkumar.srv@gmail.com>
2015-06-23 20:30:21 +0530

    Update UI.js

--- HTML AngularJS/EScript/scripts/UI.js
+++ HTML AngularJS/EScript/scripts/UI.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
   $(window).scroll(function() {
-    if ($("nav").offset().top > 50) {
-        $("nav").addClass("top-nav-collapse");
-    } else {
-        $("nav").removeClass("top-nav-collapse");
-    }
+    alert('hello world');
 });
 });
