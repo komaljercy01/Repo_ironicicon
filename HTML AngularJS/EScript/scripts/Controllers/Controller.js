90391e9544d33abacb6e5b7173bda7e0a2cc4cf5
ironicicon <ramkumar.srv@gmail.com>
2015-06-25 22:11:24 +0530

    Update Controller.js

--- HTML AngularJS/EScript/scripts/Controllers/Controller.js
+++ HTML AngularJS/EScript/scripts/Controllers/Controller.js
@@ -10,7 +10,7 @@ var controllers=angular.module('MainApp.controllers',[]);
             $scope.Message = "Enter your Username and password to access the Files";
             $scope.TestMethod=function(){
                $window.alert("poda") ;
-            }
+            };
     });
     controllers.controller('ContactUsController', function ($scope) {
             $scope.Message = "Call us or mail us.. but we wont give details";
