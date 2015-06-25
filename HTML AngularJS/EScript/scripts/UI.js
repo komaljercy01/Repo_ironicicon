1e158dc46da0e27e590b2c39c1f753611456e51e
ironicicon <ramkumar.srv@gmail.com>
2015-06-25 19:40:10 +0530

    Update UI.js

--- HTML AngularJS/EScript/scripts/UI.js
+++ HTML AngularJS/EScript/scripts/UI.js
@@ -4,8 +4,16 @@ $(document).ready(function(){
     if($( window ).width()<975)
     {
       $(window).scroll(function(e) {
-       $('.responsive').css('margin-top','200px');
-       $('.leftNav').css('display','block');
+      if(e.currentTarget.innerWidth<975)
+        {
+          $('.responsive').css('margin-top','200px');
+          $('.leftNav').css('display','block');
+        }
+        else
+        {
+          $('.responsive').css('margin-top','0px');
+        }
+        }
       });
     }
   });
